1879ad5e41d99457154e7795c85c3215208746ed
abdullahbakla7323 <153926191+abdullahbakla7323@users.noreply.github.com>
2024-11-03 16:29:04 +0300

    Update ex06.c

--- ex06.c
+++ ex06.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int divisible_by_3[20]; 
+    int index = 0;
+
+    for (int i = 100; i >= 50; i--) {
+        if (i % 3 == 0) {
+            divisible_by_3[index] = i;
+            index++;
+        }
+    }   
+    printf("Result:\n");
+    for (int j = 0; j < index; j++) {
+        printf("%d ", divisible_by_3[j]);
+    }
+
+    return 0;
+}
 
