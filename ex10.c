744b7e6ad281e0a1bd93820856479013f99bba30
abdullahbakla7323 <153926191+abdullahbakla7323@users.noreply.github.com>
2024-11-03 17:03:55 +0300

    Update ex10.c

--- ex10.c
+++ ex10.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    for (i = 1; i <= 10; i++) {
+        for (j = 1; j <= 10; j++) {
+          
+            printf("%d", i * j);
+            if (j < 10) {
+                printf(", ");
+            }
+        }
+        printf("\n"); 
+    }
+
+    return 0;
+}
 
