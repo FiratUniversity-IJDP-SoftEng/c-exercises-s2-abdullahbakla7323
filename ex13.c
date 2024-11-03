e5e652bb78d893d73e85fa8434b5cdb8327dabae
abdullahbakla7323 <153926191+abdullahbakla7323@users.noreply.github.com>
2024-11-03 17:26:35 +0300

    Update ex13.c

--- ex13.c
+++ ex13.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+
+ long factorial(int num) {
+  
+    if (num == 0 || num == 1) {
+        return 1;
+    } else {
+
+        return num * factorial(num - 1);
+    }
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number (less than 50): ");
+    scanf("%d", &num);
+
+  
+    if (num < 0 || num >= 50) {
+        printf("Please enter a  number less than 50.\n");
+    } else {
+  
+      long result = factorial(num);
+  
+        printf(" %d! = %llu\n", num, result);
+    }
+
+    return 0;
+}
 
