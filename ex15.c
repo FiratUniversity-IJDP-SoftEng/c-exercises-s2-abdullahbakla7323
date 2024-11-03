944fcf8481d4c9be91a48fbc32a2981619ac23a4
abdullahbakla7323 <153926191+abdullahbakla7323@users.noreply.github.com>
2024-11-03 17:50:03 +0300

    Update ex15.c

--- ex15.c
+++ ex15.c
@@ -1,1 +1,50 @@
+#include <stdio.h>
 
+int main() {
+    int n;
+
+
+    do {
+        printf("Please enter a number (1-10): ");
+        scanf("%d", &n);
+        if (n < 1 || n > 10) {
+     printf("oww sory!. Please enter a number between 1 and 10.\n");
+        }
+    } while (n < 1 || n > 10); 
+
+
+    for (int i = 1; i <= n; i++) {
+
+        for (int j = n; j > i; j--) {
+            printf(" ");
+        }
+
+        for (int j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        for (int j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+        printf("\n"); 
+    }
+
+
+    for (int i = n - 1; i >= 1; i--) {
+
+        for (int j = n; j > i; j--) {
+            printf(" ");
+        }
+
+        for (int j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        for (int j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+        printf("\n"); 
+    }
+
+    return 0;
+}
