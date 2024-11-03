544d2ceea8658f9e16ccea97bad2c75dbcd7eb3b
abdullahbakla7323 <153926191+abdullahbakla7323@users.noreply.github.com>
2024-11-03 16:40:41 +0300

    Update ex08.c

--- ex08.c
+++ ex08.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int main() {
+    int numbers[10];  
+    int total = 0;
+    float average;
+ 
+    printf("Please enter 10 numbers:\n");
+    for (int i = 0; i < 10; i++) {
+        printf("Number %d: ", i + 1);
+        scanf("%d", &numbers[i]);
+        total += numbers[i];  
+    }
+    printf("\nAll numbers:\n");
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", numbers[i]);
+    }
+    printf("\n"); 
+    average = total / 10.0; 
+    printf("\nTotal: %d\n", total);
+    printf("Average: %.2f\n", average);
+
+    return 0;
+}
