70634c32f3cc839afd7093deda60156964b5270a
abdullahbakla7323 <153926191+abdullahbakla7323@users.noreply.github.com>
2024-11-03 17:19:39 +0300

    Update ex12.c

--- ex12.c
+++ ex12.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+   long factorial = 1; 
+    printf("Please enter a number (less than 50): ");
+    scanf("%d", &num);
+
+  
+    if (num < 0 || num >= 50) {
+        printf("Please enter a  number less than 50.\n");
+    } else {
+      
+        for (int i = 1; i <= num; i++) {
+            factorial *= i;
+        }
+      
+        printf("Factorial of %d is %llu\n", num, factorial);
+    }
+
+    return 0;
+}
 
