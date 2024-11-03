36982197d9411d30ec79d3ce26c332b964b44845
abdullahbakla7323 <153926191+abdullahbakla7323@users.noreply.github.com>
2024-11-03 16:51:15 +0300

    Update ex09.c

--- ex09.c
+++ ex09.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+int main() {
+    int number, count = 0, total = 0;
+    float average;
+
+    printf("Enter numbers (It stops when you press -1):\n");
+        
+
+    while (1) {
+      
+        printf("Number: ");
+        scanf("%d", &number);
+
+        
+        if (number == -1) {
+            break;
+        }
+        count++;
+        total += number;
+    }  
+    if (count > 0) {
+        average = (float)total / count;
+    } else {
+        average = 0.0;
+    }
+    printf("\nAll numbers entered (excluding -1): %d\n", count);
+    printf("Total of numbers: %d\n", total);
+    printf("Average of numbers: %.2f\n", average);
+
+    return 0;
+}
